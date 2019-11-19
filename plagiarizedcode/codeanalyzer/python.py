bf07f0f013d81aeb32135aa2155d38a9d210cf45
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-11-19 12:42:47 +0100

    begin python analysis

--- plagiarizedcode/codeanalyzer/python.py
+++ plagiarizedcode/codeanalyzer/python.py
@@ -7,3 +7,10 @@ class PythonFile(CodeFile):
     """Python code file."""
 
     supported_extensions = ("py",)
+
+    def _get_text(self):
+        with open(self.path, "r", encoding="utf-8") as f:
+            return f.read()
+
+    def _get_normalized_text(self):
+        return self.text
