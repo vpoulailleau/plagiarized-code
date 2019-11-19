472c93093757c5f2cd0452aac89ca5a863875f07
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-11-19 11:09:58 +0100

    argparse

--- plagiarizedcode/main.py
+++ plagiarizedcode/main.py
@@ -1,2 +1,30 @@
+"""CLI for plagiarizedcode."""
+
+import argparse
+
+import simplelogging
+
+
 def main():
-    print("hello")
+    """Entry point."""
+    log = simplelogging.get_logger("__main__")
+
+    parser = argparse.ArgumentParser(description="Linter for *.po files.")
+    parser.add_argument("-v", "--verbose", action="count", default=0)
+    parser.add_argument(
+        "-i",
+        "--input-path",
+        metavar="PATH",
+        type=str,
+        help="path of the directory to check",
+        default=".",
+    )
+    args = parser.parse_args()
+    if args.verbose < 1:
+        log.reduced_logging()
+    elif args.verbose < 2:
+        log.normal_logging()
+    else:
+        log.full_logging()
+
+    log.info("Starting analysis of %s", str(args.input_path))
