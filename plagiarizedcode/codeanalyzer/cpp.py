9f97c21ec26c5fb353eeed25cd59f1b7c1720222
Vincent Poulailleau <vpoulailleau@gmail.com>
2022-04-01 12:01:37 +0200

    parse C file

--- plagiarizedcode/codeanalyzer/cpp.py
+++ plagiarizedcode/codeanalyzer/cpp.py
@@ -2,8 +2,12 @@
 
 import re
 
+import simplelogging
+
 from plagiarizedcode.codeanalyzer.codeanalyzer import CodeFile
 from plagiarizedcode.codeanalyzer.utils import parenthesis_in_one_line
+
+log = simplelogging.get_logger()
 
 
 class CppFile(CodeFile):
@@ -21,14 +25,54 @@ class CppFile(CodeFile):
             line = re.sub(r"//.*$", "", line)
             line = line.rstrip()
             if line:
-                lines.append(line)
-        text = "\n".join(lines)
-        text = parenthesis_in_one_line(text)
-        # TODO normalize {}
+                if line.startswith("#") and not line.endswith("\\"):
+                    lines.append(line + "\n")
+                else:
+                    lines.append(line)
+        text = " ".join(lines)
+        text = re.sub(r"[ \t]+", " ", text)
+
         return text
 
     def _get_blocks(self) -> list[str]:
         result = [""]
+        content = [""]
+        text = self._get_normalized_text()
+        for line in text.splitlines():
+            line = line.lstrip()
+            if not line:
+                continue
+
+            while line:
+                old_line = line
+
+                # preprocessor
+                if line.startswith("#"):
+                    if content[-1].startswith("#"):
+                        content[-1] += "\n" + line
+                    else:
+                        content.append(line)
+                    break
+
+                match = ""
+                level = 0
+                for char in line:
+                    match += char
+                    if char == "{":
+                        level += 1
+                    if char == "}":
+                        level -= 1
+                    if level == 0 and char == ";":
+                        break
+                content.append(match.lstrip())
+                line = line[len(match) :]
+
+                if old_line == line:
+                    log.error("unparsed text: >>>%s", line)
+                    break
+
+        log.warning("\n\n".join(content))
+
         # TODO
         # for line in self.normalized_text.splitlines():
         #     if line.endswith("{"):
